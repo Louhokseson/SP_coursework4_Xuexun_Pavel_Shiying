3b2587130f826c75128e482a1cb380472d7541d1
LU Xuexun <s2160622@ed.ac.uk>
2021-11-13 14:41:57 +0000

    Xuexun Lu upload some progress

--- WG45P4.R
+++ WG45P4.R
@@ -16,6 +16,27 @@ rb <- function(theta,getg=FALSE,k=10) {
 } ## rb
 
 # Hokseson's practice
-myfunction <- function(a,b) {
- a+b
+myfunction <- function(theta,a,b) {
+ a+b+theta
 } ## rb
+
+Test_bfgs <- function(theta,f,...,tol=1e-5,fscale=1,maxit=100){
+  # Here, we need use the three dots to input our variable into the funciton f
+  result <- f(theta,...) 
+  return (result)
+}
+
+
+Test_bfgs(theta=3,myfunction,a=5,b=6)
+
+fit <- optim(c(-1,2),rb,getg = FALSE,hessian=TRUE)
+
+bfgs <- function(theta,f,...,tol=1e-5,fscale=1,maxit=100){
+  
+  number_par <- length(theta) # the number of parameters
+  # BFGS method
+  # Initialization of B as a identity matrix
+  B <- diag(number_par)
+  QN_step = first_derivative(f)
+  return (result)
+}
